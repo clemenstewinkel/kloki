7aebdbbe3c4aaaa2e676652197fe91defad0930f
clemens <clemens.tewinkel@gmail.com>
2020-05-18 20:01:24 +0200

    WordCreatorService verbessert

--- src/Service/WordCreatorService.php
+++ src/Service/WordCreatorService.php
@@ -23,6 +23,9 @@ class WordCreatorService
     public function createWord(KloKiEvent $event)
     {
         $source = $this->params->get('odt_directory');
+        $tempdir = trim(shell_exec('mktemp -d'));
+        shell_exec("cp -r $source/* $tempdir/");
+
         try {
             $template = $this->twig->loadTemplate('klo_ki_event/mietvertrag_content.xml.twig');
         } catch (Exception $e) {
@@ -33,7 +36,9 @@ class WordCreatorService
         } catch (Throwable $e) {
             return false;
         }
-        file_put_contents($source . 'content.xml', $renderedDoc);
-        return shell_exec("cd $source; zip -rq - *");
+        file_put_contents($tempdir . '/content.xml', $renderedDoc);
+        $file_content =  shell_exec("cd $tempdir; zip -rq - *");
+        shell_exec("rm -rf $tempdir");
+        return $file_content;
     }
 }
